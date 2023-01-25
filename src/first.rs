e16458b50ebbedd0d4139671dd0b50462c706c53
ongaeshi <ongaeshi0621@gmail.com>
2023-01-26 01:25:04 +0900

    feat: 仮の関数

---
+++ src/first.rs
@@ -0,0 +1,11 @@
+fn sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn compare_andswap(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
