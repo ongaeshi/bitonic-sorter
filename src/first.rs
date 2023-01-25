947bbf9e1264421b8085cb0fe374cf4c12402a44
ongaeshi <ongaeshi0621@gmail.com>
2023-01-26 01:33:32 +0900

    feat: first版実装

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,26 @@
 fn sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 { 
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
 }
 
-fn compare_andswap(x: &mut [u32], up: bool) {
-    unimplemented!();
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i); // 標準関数
+        }
+    }
 }
