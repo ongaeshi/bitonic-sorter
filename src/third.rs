2cba8e572ec5f97f3c7bfe090b53d54007db482c
ongaeshi <ongaeshi0621@gmail.com>
2023-01-31 02:46:40 +0900

    feat: Student型定義＆テスト

--- src/third.rs
+++ src/third.rs
@@ -45,6 +45,23 @@ mod tests {
     use super::sort; // 親モジュールのsort関数を利用する。
     use crate::SortOrder::*;
 
+    struct Student {
+        first_name: String,
+        last_name: String,
+        age: u8
+    }
+    
+    impl Student {
+        fn new(first_name: &str, last_name: &str, age: u8) -> Self {
+            Self {
+                first_name: first_name.to_string(),
+                last_name: last_name.to_string(),
+                age, // フィールド変数が同じ名前のときは省略形で書ける
+            }
+        }
+
+    }
+
     #[test]
     fn sort_u32_ascending() {
         // 要素数が2のべき乗じゃないと駄目
@@ -71,5 +88,23 @@ mod tests {
     fn sort_to_fail() {
         let mut x = vec![10, 30, 11];
         assert!(sort(&mut x, &Ascending).is_err());
+    }
+
+    #[test]
+    fn sort_stdudents_by_age_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
+
+        assert_eq!(
+            sort_by(&mut x, &|a, b| a.age.cmp(&b.age)),
+            Ok(())
+        );
+
+        assert_eq!(x, expected);
     }
 }
